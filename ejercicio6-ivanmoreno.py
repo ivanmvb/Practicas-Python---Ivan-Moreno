12c58ef7951628a93733350c638c2a275ea38347
Ivan <ivannmoreno2001@gmail.com>
2020-04-29 20:57:02 +0200

    Ultimo Cambio

--- ejercicio6-ivanmoreno.py
+++ ejercicio6-ivanmoreno.py
@@ -5,29 +5,25 @@ Created on Tue Apr 28 19:29:44 2020
 @author: ivann
 """
 
-abecedario = 'abcdefghijklmnñopqrstuvwxyz'
+abecedario= ' abcdefghijklmnñopqrstuvwxyz '
 
-print("MI CIFRADO CÉSAR")
-texto = input("Escribe el texto a cifrar:")
-clave = int(input("Escribe la clave de cifrado (un número del 1 al 27):"))
+print("Bienvenidos a mi cifrador César")
 
+texto_claro=input("Introduce una frase para cifrarla: ")
+clave=int(input("Introduce un numero para cifrar(Un número del 1 al 27):"))
 
 texto_cifrado=""
 
-for letra in texto:
-    nueva_posicion = abecedario.find (letra) + clave
-    letra_cifrada = int(nueva_posicion) % len(abecedario)
-    texto_cifrado = texto_cifrado + str(abecedario [letra_cifrada])
-print("\nEl mensaje cifrado es:",texto_cifrado)
 
-
+for letra in texto_claro:
+    nueva_posicion=abecedario.find(letra)+ clave
+    letra_cifrada=int(nueva_posicion)%len(abecedario)
+    texto_cifrado= texto_cifrado+str(abecedario[letra_cifrada])
+print ("El mensaje cifrado es:" ,texto_cifrado)
+print ("=====================================================")
 texto_descifrado=""
-
 for letra in texto_cifrado:
-    nueva_posicion = abecedario.find (letra) - clave
-    letra_cifrada = int(nueva_posicion) % len(abecedario)
-    texto_descifrado = texto_descifrado + str(abecedario[letra_cifrada])
-
-print("\nEl mensaje descifrado es:",texto_descifrado)
-
-
+    nueva_posicion= abecedario.find(letra)-clave
+    letra_cifrada= int(nueva_posicion)%len(abecedario)
+    texto_descifrado= texto_descifrado+str(abecedario[letra_cifrada])
+print ("El mensaje descifrado es: " ,texto_descifrado)
--- ejercicio7-ivanmoreno.py
+++ ejercicio7-ivanmoreno.py
@@ -5,3 +5,27 @@ Created on Tue Apr 28 19:31:59 2020
 @author: ivann
 """
 
+abecedario= ' abcdefghijklmnñopqrstuvwxyz '
+
+print("Bienvenidos a mi cifrador César")
+
+texto_claro=input("Introduce una frase para cifrarla: ")
+clave=int(input("Introduce un numero para cifrar(Un número del 1 al 27):"))
+
+
+def cifracesar (texto_cifrado,clave):
+    texto_cifrado=""
+
+
+for letra in texto_claro:
+    nueva_posicion=abecedario.find(letra)+ clave
+    letra_cifrada=int(nueva_posicion)%len(abecedario)
+    texto_cifrado=texto_cifrado+str(abecedario[letra_cifrada])
+print ("El mensaje cifrado es:" ,texto_cifrado)
+print ("=====================================================")
+texto_descifrado=""
+for letra in texto_cifrado:
+    nueva_posicion= abecedario.find(letra)-clave
+    letra_cifrada= int(nueva_posicion)%len(abecedario)
+    texto_descifrado= texto_descifrado+str(abecedario[letra_cifrada])
+print ("El mensaje descifrado es: " ,texto_descifrado)
